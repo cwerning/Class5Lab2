f3637d7a727830434a41af7da62b6913b121385e
cwerning <cwerning@10.18.12.26>
2017-09-12 15:31:02 -0500

    Added all necessary classes to tasks

---
+++ src/class5lab2/task2/Business.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package class5lab2.task2;
+
+/**
+ *
+ * @author cwerning
+ */
+public class Business {
+    
+}
