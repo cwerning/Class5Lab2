6213cdc24f1ad103221088be9e4cca697f56dc69
L115student <L115student@L115-08-043627.bitnet.wctc.edu>
2017-09-14 12:46:12 -0500

    Finished abstract

--- src/class5lab2/task2/Business.java
+++ src/class5lab2/task2/Business.java
@@ -9,6 +9,67 @@ package class5lab2.task2;
  *
  * @author cwerning
  */
-public class Business {
+public abstract class Business {
+    
+    private String businessName;
+    private String[] ownerNames;
+    private int numberOfEmployees;
+    private double dayRevenue;
+    private double weekRevenue;
+    private double monthRevenue;
+    
+    public abstract String getTypeOfBusiness();
+
+    public double getDayRevenue() {
+        return dayRevenue;
+    }
+
+    public void setDayRevenue(double dayRevenue) {
+        this.dayRevenue = dayRevenue;
+    }
+
+    public double getWeekRevenue() {
+        return weekRevenue;
+    }
+
+    public void setWeekRevenue(double weekRevenue) {
+        this.weekRevenue = weekRevenue;
+    }
+
+    public double getMonthRevenue() {
+        return monthRevenue;
+    }
+
+    public void setMonthRevenue(double monthRevenue) {
+        this.monthRevenue = monthRevenue;
+    }
+    
+    public String getBusinessName() {
+        return businessName;
+    }
+
+    public void setBusinessName(String businessName) {
+        this.businessName = businessName;
+    }
+
+    public String[] getOwnerNames() {
+        return ownerNames;
+    }
+
+    public void setOwnerNames(String[] ownerNames) {
+        this.ownerNames = ownerNames;
+    }
+
+    public int getNumberOfEmployees() {
+        return numberOfEmployees;
+    }
+
+    public void setNumberOfEmployees(int numberOfEmployees) {
+        this.numberOfEmployees = numberOfEmployees;
+    }
+    
+    
+    
+    
     
 }
